6c3cad4b2acc760be30e3d1c2f07a75f46faddd8
PHAGE-Official <100456211+PHAGE-Official@users.noreply.github.com>
2022-09-20 19:25:22 +0800

    Add files via upload

---
+++ src/X-Basic.js
@@ -0,0 +1,54 @@
+/*
+	======================
+	X-Ray.JS 
+	-------------------------------------
+	Description:
+		A Lite Web Application
+		Development Package Build
+		With Pure Javascript
+	-------------------------------------
+	This File:
+		This File is Used to Create
+		Basic HTML & DOM
+	-------------------------------------
+	Last Modify: 2022/9/20-19:00
+	-------------------------------------
+	Author: Ethan Shen
+	-------------------------------------
+	Version: 0.0.0.01_dev_Alpha
+	-------------------------------------
+	Copy Right
+	(C) 2022 NLTech Coporation
+	======================
+*/
+
+// Use Stirct Mode
+"use strict";
+
+let Get_Version = () =>{
+	// Version Information Function
+	const Version = ["0.0.0.01_dev_Alpha","Build 04"];
+	return Version;
+}
+
+function Create_Element(Base,Target,Content){
+	let Base_Obj = document.getElementsByTagName(Base)[0];
+	let Append_Obj = document.createElement(Target);
+	let Text_Node = document.createTextNode(Content);
+	Append_Obj.appendChild(Text_Node);
+	Base_Obj.appendChild(Append_Obj);
+	return Append_Obj;
+}
+
+function Get_Device_Info(){
+	let Device =[];
+	Device[0] = navigator.userAgent;
+	Device[1] = navigator.platform;
+	Device[2] = window.screen.width+'x'+window.screen.height;
+	return Device;
+}
+
+function Attribute(Object,Selector,Name){
+	Attr = Object.setAttribute(Selector,Name);
+	return Attr;
+}
