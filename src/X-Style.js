677233695ba7b4d5cad450051f5d6f850d4df4f6
PHAGE-Official <100456211+PHAGE-Official@users.noreply.github.com>
2022-09-25 10:57:59 +0800

    Update X-Style.js

--- src/X-Style.js
+++ src/X-Style.js
@@ -1,3 +1,26 @@
+/*
+	======================
+	X-Ray.JS 
+	-------------------------------------
+	Description:
+		A Lite Web Application
+		Development Package Build
+		With Pure Javascript
+	-------------------------------------
+	This File:
+		This File is Used to Setup
+		Element's Style
+	-------------------------------------
+	Last Modify: 2022/9/25-10:57
+	-------------------------------------
+	Author: Ethan Shen
+	-------------------------------------
+	Version: 0.0.0.01_dev_Alpha
+	-------------------------------------
+	Copy Right
+	(C) 2022 NLTech Coporation
+	======================
+*/
 function Get_Style(Element){
 	let style = Element.style;
 	return style;
