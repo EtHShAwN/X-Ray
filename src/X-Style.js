2c47204d3fa9882a1443589f2b2d658dd2947877
Ethan Shen <100456211+Phage-Official@users.noreply.github.com>
2022-09-26 18:48:35 +0800

    Add files via upload

--- src/X-Style.js
+++ src/X-Style.js
@@ -1,24 +1,7 @@
-/*
-	======================
-	X-Ray.JS 
-	-------------------------------------
-	Description:
-		A Lite Web Application
-		Development Package Build
-		With Pure Javascript
-	-------------------------------------
-	This File:
-		This File is Used to Setup
-		Element's Style
-	-------------------------------------
-	Last Modify: 2022/9/25-10:57
-	-------------------------------------
-	Author: Ethan Shen
-	-------------------------------------
-	Version: 0.0.0.01_dev_Alpha
-	======================
-*/
 function Get_Style(Element){
 	let style = Element.style;
 	return style;
+}
+function Set_Style(Element){
+	return;
 }
