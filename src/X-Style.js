cfd06a01788b98f0da2f915cea39ff96af3ab3ee
Ethan Shen <100456211+Phage-Official@users.noreply.github.com>
2022-10-30 14:55:13 +0800

    Delete X-Style.js

--- src/X-Style.js
+++
@@ -1,9 +0,0 @@
-function Get_Style(Element){
-	let style = Element.style;
-        let VStyle = (style.color,style.font);
-	console.log(VStyle);
-	return style;
-}
-function Set_Style(Element){
-	return;
-}
