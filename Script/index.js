6521cabce928d4290db61780c4b6c78659b895ff
Ethan Shen <100456211+Phage-Official@users.noreply.github.com>
2022-10-31 22:07:23 +0800

    Add files via upload

--- Script/index.js
+++ Script/index.js
@@ -2,13 +2,21 @@ title = Create('head','title');
 title.textContent = "X-Ray";
 Headline = Create('body','h1');
 Headline.textContent = "X-Ray Framework";
-Device_Info = Device();
-if (Device_Info[1] == undefined){
+Button = Create('body','button');
+Button.textContent = "Detect Your Device Information";
+Set_Element_Attiributes(Button,'id','CPU_Info');
+Set_Element_Attiributes(Button,'onclick','Device_Info()');
+function Device_Info() {
+	Device_Value = Device();
 	CPU_Core_Info = Create('body','div');
-	CPU_Core_Info.textContent = 'X-Ray Has NO Access to Your Processor Info';
+	if (Device_Value[1] == undefined){
+		CPU_Core_Info.textContent = 'X-Ray Has NO Access to Your Processor Info';
+	}
+	else{
+		CPU_Core_Info.textContent = ('Your Processor has '+
+									Device_Value[1]+
+									' Concurrency Core');
+	}
+	Set_Element_Attiributes(CPU_Core_Info,'id','CPU_Core_Info')
+	Set_Element_Attiributes(Button,'onclick','')
 }
-else{
-	CPU_Core_Info = Create('body','div');
-	CPU_Core_Info.textContent = 'Your Processor has '+Device_Info[1]+' Concurrency Core';
-}
-Set_Element_Attiributes(CPU_Core_Info,'id','CPU_Info');
